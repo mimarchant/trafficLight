285c35fa8c2329686d6d8f8e743b06ba42274bbf
mimarchant <mimarchtt@gmail.com>
2021-01-18 14:27:24 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isColorSelected: " "
+    };
+  }
+
+
+
+
+
+  render() {
+    let red2 = " ";
+    let orange2 = " ";
+    let green2 = " ";
+
+    if (this.state.isColorSelected === "red")
+      red2 = "glow"
+    if (this.state.isColorSelected === "orange")
+      orange2 = "glow"
+    if (this.state.isColorSelected === "green")
+      green2 = "glow";
+
+
+    return (
+      <div className="container fondo">
+        <div>
+
+        </div>
+        <div>
+          <div onClick={() => this.setState({ isColorSelected: "red" })} className={'redlight ' + red2}>
+
+          </div>
+          <div onClick={()=>this.setState({ isColorSelected: "orange" })} className={'orangelight ' + orange2}>
+
+          </div>
+          <div onClick={()=>this.setState({ isColorSelected: "green" })} className={'greenlight ' + green2}>
+
+          </div>
+        </div>
+      </div >
+    )
+  }
+};
 
 export default App;
